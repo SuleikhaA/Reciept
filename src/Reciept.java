bc834441ffe963bf213a443ea60b45796e6f6856
Suleikha A <suleikha@Suleikhas-MacBook-Air.local>
2017-10-02 19:48:35 -0400

    Initial commit

---
+++ src/Reciept.java
@@ -0,0 +1,16 @@
+public class Reciept {
+    +------------------------------------+
+    |        Java Bank ATM Reciept       |
+    |     Wednesday, December 2, 2015    |
+    |           ATM Location #123        |
+    |                                    |
+    |                                    |
+    |        Account Number: 1234567     |
+    |        Customer: Sue E. Ocean      |
+    |       Transaction Type: Deposit    |
+    |     Transcation Amount: $500.00    |
+    |     Account Balance: $1,5000.00    |
+    |     Thank You for banking with us  |
+    |          Have a coffee day         |
+    +------------------------------------+
+}
